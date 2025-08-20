42aff5d470c5e0fe8f0974ded5a23d3ed4c69138
Fahim Raiyan <fahim.cse.20220204098@aust.edu>
2025-08-20 09:26:10 +0600

    Updated profile

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Profile.css";
-import pic from "../../pic.jpg"
-
+import pic from "../../pic.jpg";
+import {FaEnvelope,FaPhone,FaBirthdayCake} from "react-icons/fa";
 function Profile() {
   return (
     <div className="profile-container">
@@ -9,15 +9,28 @@ function Profile() {
         <img
           src={pic}
           alt="Profile"
-          style={{ width: "100px", height: "100px", borderRadius: "50%", objectFit: "cover" }}
+          style={{
+            width: "100px",
+            height: "100px",
+            borderRadius: "50%",
+            objectFit: "cover",
+          }}
         />
         <h2 className="profile-name">Fahim Raiyan Ahmed</h2>
-        <p className="profile-email">E-mail: fahim.cse.20220204098@aust.cse</p>
-        <p className="profile-joined">Joined: January 10, 2023</p>
-        <p className="profile-dob">Date of birth : June 17, 2003</p>
+        <p className="profile-email">
+          <FaEnvelope className="profile-icon" />
+          <strong> E-mail :</strong> fahim.cse.20220204098@aust.cse
+        </p>
+        <p className="profile-mob">
+          <FaPhone className="profile-icon" />
+          <strong> Phone :</strong> +8801851500036
+        </p>
+        <p className="profile-dob">
+          < FaBirthdayCake className="profile-icon" />
+         <strong> Date of birth :</strong> June 17, 2003
+        </p>
       </div>
     </div>
   );
 }
-
 export default Profile;
